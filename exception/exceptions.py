8b72cad7d66b3df1e5efb8ded62105993b75d8e0
Gaurav Tiwari <gauravtiwari01@gmail.com>
2024-08-14 23:27:05 -0400

    Running it as distributed system

--- exception/exceptions.py
+++ exception/exceptions.py
@@ -3,3 +3,9 @@ class NoDataFoundException(Exception):
     This exception will be raised when no data is found in the system
     """
     pass
+
+class UnauthorizedRequestException(Exception):
+    """
+    The exception to be thrown when a request hits the follower directly
+    """
+    pass
