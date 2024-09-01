ff5efe5cb30f7076aee6f1311a1231479a68d757
Gaurav Tiwari <gauravtiwari01@gmail.com>
2024-09-01 00:41:34 -0400

    Fixed issues with the env specific settings

--- config.py
+++ config.py
@@ -7,10 +7,3 @@ settings = Dynaconf(
     environments = True,
     apply_default_on_none = True
 )
-
-print(settings.server.ip)  # Will print "127.0.0.1" for development, "0.0.0.0" for production, or the default value
-print(settings.zooKeeper.host)  # Will print "localhost"
-print(settings.dataDirectory)
-
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load these files in the order.
